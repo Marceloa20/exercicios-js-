6f2ac79c9b431547f88f99876417494a5e98498f
MARCELO MOTTA AVINTE <MARCELO0079@DIGITALCOLLEGE.LOCAL>
2023-10-20 19:33:46 -0300

    exercício 6

---
+++ exercicio6.js
@@ -0,0 +1,14 @@
+let a = true
+let b = false
+
+if (a == true) {
+    console.log("Verdadeiro")
+} else {
+    console.log("Falso")
+}
+
+if (b == true) {
+    console.log("Verdadeiro")
+} else {
+    console.log("Falso")
+}
